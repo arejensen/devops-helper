3113274a83e9f27d339dce3f82296d2d6f052cb8
Are Jensen <arejensen@gmail.com>
2025-02-13 18:30:52 +0100

    Fix config pathname location algorithm

--- environment-variables.lisp
+++ environment-variables.lisp
@@ -8,7 +8,7 @@
 
 (defun init-config ()
   "Read environment variables from config.env at runtime."
-  (cl-env:init "config.env")
+  (cl-env:init (merge-pathnames (uiop:pathname-directory-pathname sb-ext:*core-pathname*) "config.env"))
   (setf *organization* (cl-env:getenv "DEVOPS_ORGANIZATION")
         *pat*          (cl-env:getenv "DEVOPS_USER_PAT")
         *user-email*   (cl-env:getenv "DEVOPS_USER_EMAIL")))
